753e135b790497deb88d69db29f2d6116d031a60
CastilloRmz <guadalupe.castillo17@tectijuana.edu.mx>
2020-11-29 04:16:45 -0800

    Start Proyect

---
+++ Makeup2.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Makeup2.Web.MvcApplication" Language="C#" %>
---
+++ Makeup2.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Makeup2.Web.Startup))]
+namespace Makeup2.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
